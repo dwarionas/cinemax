3bd8997142357ddedd511258758451a035007016
dwarionas <che4tez@gmail.com>
2022-12-30 15:52:10 +0200

    layout and a little bit of functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Outlet } from "react-router";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import './styles/app.scss'
+import NavAside from "./components/NavAside";
+
+const App: React.FC = () => {
+    return (
+        <div className={'app'}>
+            <NavAside/>
+
+            <main className={'app__content'}>
+                <Outlet/>
+            </main>
+        </div>
+    );
+};
 
 export default App;
