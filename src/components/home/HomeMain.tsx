1fec86403116d85c1fc578052aa66485fad4317e
dwarionas <che4tez@gmail.com>
2023-01-04 19:12:54 +0200

    star rating added

--- src/components/home/HomeMain.tsx
+++ src/components/home/HomeMain.tsx
@@ -1,16 +1,31 @@
 import React from 'react';
 import { ISliderData } from "../../redux/slices/homeSlice";
+import { Star } from "../Icons";
 
 interface IProps {
     item: ISliderData;
 }
 
 const HomeMain: React.FC<IProps> = ({ item }) => {
+    const yellow = '#F9B601';
+    const grey = '#9b9b9b';
+
     return (
         <div className={'home__main'} >
             <span className={'home__main-header'}>{item.title || item.name}</span>
             <span className={'home__main-season'}>Season 1</span>
-            <span className={'home__main-stars'}>Rate: {item.vote_average.toFixed(1)} ({item.vote_count} voices)</span>
+            <div className={'home__main-stars'}>
+                {
+                    [...Array(Math.round(item.vote_average / 2))].map(x => ++x).map(() => (
+                        <Star color={yellow} />
+                    ))
+                }
+                {
+                    [...Array(5 - Math.round(item.vote_average / 2))].map(x => ++x).map(() => (
+                        <Star color={grey} />
+                    ))
+                }
+            </div>
             <span className={'home__main-genre'}>Crime | Drama | Mystery</span>
             <div className={'home__main-buttons'}>
                 <button className={'home__main-buttons-watch'}>{'>'}</button>
